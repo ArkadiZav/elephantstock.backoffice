99cbf922c4df74ff66570098fea434d3ee1089b6
Arkadi <mrarkadi@gmail.com>
2017-07-26 17:32:12 +0300

    set up node for initial hello world

---
+++ server.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+//mongoose.connect(process.env.CONNECTION_STRING||'mongodb://localhost/emunah');
+
+var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(express.static('public'));
+app.use(express.static('node_modules'));
+
+app.all('[^.]+', function(req, res) {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+app.listen(process.env.PORT || '8000', function(){
+  console.log("8000. Elephantstock")
+});
